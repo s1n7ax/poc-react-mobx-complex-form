4bba12d0877756b23b26af64aafb462b0ef7a895
s1n7ax <srineshnisala@gmail.com>
2024-11-06 10:45:48 +0530

    feat: add submit button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,27 @@
 import { GroupComponentState } from "@/lib/store/GroupComponentStore";
 import { observer } from "mobx-react-lite";
 import GenericComponentList from "./GenericComponentList";
+import { Button } from "@mui/material";
 
 export interface FormProps {
   data: GroupComponentState;
 }
 
 const Form = observer(({ data }: FormProps) => {
-  console.log("rendering::Form", data);
+  console.log("rendering::Form");
 
   return (
     <form className="p-5 border-2 border-black m-2">
       <GenericComponentList data={data} />
+      <Button
+        variant="outlined"
+        disabled={data.hasError}
+        sx={{
+          m: "2rem",
+        }}
+      >
+        Submit
+      </Button>
     </form>
   );
 });
